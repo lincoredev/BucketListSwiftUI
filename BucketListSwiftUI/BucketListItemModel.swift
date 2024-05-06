bc88237baca5133b5252ca6d964744d29d1de49b
Vova Robot <lincore14@mail.ru>
2024-05-06 14:58:26 +0300

    name change String

--- BucketListSwiftUI/BucketListItemModel.swift
+++ BucketListSwiftUI/BucketListItemModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct BucketListItemModel: Identifiable, Equatable, Codable {
-    var name: Int
+    var name: String
     var id = UUID()
     var isCompleted: Bool = false
 }
