0b4c0b6aa44b994af8286f75ad752202ecb3b5da
Vova Robot <lincore14@mail.ru>
2024-05-06 15:02:41 +0300

    test commit

--- BucketListSwiftUI/BucketListItemModel.swift
+++ BucketListSwiftUI/BucketListItemModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct BucketListItemModel: Identifiable, Equatable, Codable {
-    var name: String
+    var name: Double
     var id = UUID()
     var isCompleted: Bool = false
 }
